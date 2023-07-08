d3e0c5ecf8fdc68a98e315cebe800705dfc51bce
Ed <harrised94@gmail.com>
2023-07-08 11:09:22 +0100

    Init

---
+++ src/Set.tsx
@@ -0,0 +1,18 @@
+import Counters from "./Counters";
+
+import styles from "./Set.module.scss";
+
+interface SetProps {
+  set: string;
+  imageUrl?: string;
+}
+
+const Set = ({ set, imageUrl }: SetProps) => {
+  return (
+    <div className={styles.card}>
+      <Counters set={set} imageUrl={imageUrl} />
+    </div>
+  );
+};
+
+export default Set;
